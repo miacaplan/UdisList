d4b3500c1c1ba6092ae8f78e5be9f312f5aa5130
Udi Oron <udioron@gmail.com>
2016-05-26 18:49:13 +0300

    oops forgot to add migration :-)

--- ads/tests.py
+++ ads/tests.py
@@ -6,18 +6,16 @@ from . import models
 class AdsTestCase(TestCase):
     def test_ads(self):
         for i in range(20):
-
             o = models.Ad(
                 title="Puppy #{}".format(i + 1),
                 description="lovely puppy, brown and happy",
                 posted_by="Udi",
                 contact_email="udi@10x.org.il",
                 price=10 + i * 5,
-                status= models.Ad.Status.NEW if i % 2 else models.Ad.Status.APPROVED
+                status=models.Ad.Status.NEW if i % 2 else models.Ad.Status.APPROVED
             )
 
             o.full_clean()
             o.save()
-
 
         self.assertEquals(models.Ad.objects.count(), 20)
