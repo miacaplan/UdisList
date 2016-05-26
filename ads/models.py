050177b75f38e9c55a0b31a63325ed17075248f3
Udi Oron <udioron@gmail.com>
2016-05-26 17:13:48 +0300

    Add a model, a migration and a test

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Ad(models.Model):
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    posted_by = models.CharField(max_length=200)
+    contact_email = models.EmailField()
+    contact_phone = models.CharField(max_length=30,
+                                     null=True, blank=True)
+    price = models.PositiveIntegerField(null=True, blank=True)
--- ads/tests.py
+++ ads/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from . import models
+
+
+class AdsTestCase(TestCase):
+    def test_ads(self):
+        for i in range(20):
+            o = models.Ad()
+            o.title = "Puppy #{}".format(i + 1)
+            o.description = "lovely puppy, brown and happy"
+            o.posted_by = "Udi"
+            o.contact_email = "udi@10x.org.il"
+            o.price = 10 + i * 5
+            o.full_clean()
+            o.save()
+
+        self.assertEquals(models.Ad.objects.count(), 20)
